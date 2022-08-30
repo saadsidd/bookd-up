bc61167d77818d803b621e88054f842fd4766074
Saad Siddiq <siddiq.saad@gmail.com>
2022-08-30 05:11:31 -0600

    refactored nav + uninstalled styled-components

--- react-front-end/src/components/Nav.js
+++ react-front-end/src/components/Nav.js
@@ -1,13 +1,10 @@
 import React, { useContext } from "react";
-import "./styles/Nav.scss";
-import { NavLink, useNavigate } from "react-router-dom";
-
-import styled from "styled-components";
+import { useNavigate, Link } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
+import "./styles/Nav.scss";
 
 export default function Nav() {
   const { user, setUser } = useContext(UserContext);
-
   const navigate = useNavigate();
 
   const logout = () => {
@@ -17,62 +14,24 @@ export default function Nav() {
   };
 
   return (
-    <nav className="nav">
-      <div className="logo">
-        <StyledNavLink to="/">BOOK'D UP</StyledNavLink>
-      </div>
+    <nav className="nav-bar">
+      <Link to="/" className="nav-bar__logo">BOOK'D UP</Link>
 
-      <div className="links">
-        <ul>
-          <li>
-            <StyledNavLink to="/" className="nav-item">
-              Home
-            </StyledNavLink>
-          </li>
-          <li>
-            <StyledNavLink to="/search" className="nav-item">
-              Search
-            </StyledNavLink>
-          </li>
-          <li>
-            <StyledNavLink to="/match" className="nav-item">
-              MatchBook<span className="fire-emoji">🔥</span>
-            </StyledNavLink>
-          </li>
-          <li>
-            {user ? (
-              <StyledNavLink to="/profile" className="nav-item signup">
-                My Profile
-              </StyledNavLink>
-            ) : (
-              <StyledNavLink to="/signup" className="nav-item signup">
-                Sign Up
-              </StyledNavLink>
-            )}
-          </li>
-          <li>
-            {user ? (
-              <button onClick={logout} type="submit" className="login-btn">
-                Logout
-              </button>
-            ) : (
-              <StyledNavLink to="/login" className="nav-item login">
-                Login
-              </StyledNavLink>
-            )}
-          </li>
-        </ul>
+      <div className="nav-bar__links">
+        <Link to="/" className="nav-bar__item">Home</Link>
+        <Link to="/search" className="nav-bar__item">Search</Link>
+        <Link to="/match" className="nav-bar__item">MatchBook<span className="nav-bar__fire-emoji">🔥</span></Link>
+        {user ? (
+          <Link to="/profile" className="nav-bar__item">My Profile</Link>
+        ) : (
+          <Link to="/signup" className="nav-bar__item">Sign Up</Link>
+        )}
+        {user ? (
+          <button onClick={logout} type="submit" className="login-btn">Logout</button>
+        ) : (
+          <Link to="/login" className="nav-bar__item">Login</Link>
+        )}
       </div>
     </nav>
   );
 }
-
-const StyledNavLink = styled(NavLink)`
-  // text-emphasis: none;
-  // text-decoration: none;
-  // &:hover {
-  //   text-emphasis: none;
-  text-decoration: none;
-  color: black;
-  // }
-`;

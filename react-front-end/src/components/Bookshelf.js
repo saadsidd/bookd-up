d879f8872c2e81907c24da06044fa7d1c46cf09d
Saad Siddiq <siddiq.saad@gmail.com>
2022-08-30 00:28:24 -0600

    rendering books from prop after loading completes

--- react-front-end/src/components/Bookshelf.js
+++ react-front-end/src/components/Bookshelf.js
@@ -1,23 +1,24 @@
 import React from "react";
-import './Bookshelf.scss';
+import './styles/Bookshelf.scss';
 import Spinner from "./Spinner";
 
 export default function Bookshelf(props) {
 
-  const { label, isLoading } = props;
+  const { label, isLoading, books } = props;
 
   return (
     <div className="shelf__container--outer">
       <div className="shelf__container--inner">
         <div className="shelf__books-holder">
-          {isLoading && <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}><Spinner /></div>}
+          {isLoading && <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%'}}><Spinner /></div>}
+          {!isLoading && books}
         </div>
         <div className="shelf">
-          <div className="shelf__wall shelf__wall--back">back</div>
-          <div className="shelf__wall shelf__wall--right">right</div>
-          <div className="shelf__wall shelf__wall--left">left</div>
-          <div className="shelf__wall shelf__wall--top">top</div>
-          <div className="shelf__wall shelf__wall--bottom">bottom</div>
+          <div className="shelf__wall shelf__wall--back"></div>
+          <div className="shelf__wall shelf__wall--right"></div>
+          <div className="shelf__wall shelf__wall--left"></div>
+          <div className="shelf__wall shelf__wall--top"></div>
+          <div className="shelf__wall shelf__wall--bottom"></div>
         </div>
       </div>
       <div className="shelf__label">{label}</div>

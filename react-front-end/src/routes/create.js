99b1628be25ce64e881a2a11a11181d2ca4f66d5
Saad Siddiq <siddiq.saad@gmail.com>
2022-09-03 03:25:00 -0600

    added maxlength counters + image url input

--- react-front-end/src/routes/create.js
+++ react-front-end/src/routes/create.js
@@ -1,66 +1,68 @@
 import React, { useState, useContext } from "react";
-import "./styles/create.scss";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
+import Button from '../components/Button';
+import "./styles/create.scss";
 
 export default function Create() {
-  const [clubName, setClubName] = useState("");
-  const [clubDescription, setClubDescription] = useState("");
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [imageURL, setImageURL] = useState(null);
+
+  const nameCounter = 50 - name.length;
+  const descriptionCounter = 200 - description.length;
+
   const { user } = useContext(UserContext);
-
-
   const navigate = useNavigate();
 
-  const create = () => {
+  const createClub = () => {
     axios.post("/api/clubs", {
         user_id: user.id,
-        name: clubName,
-        description: clubDescription,
-        private: false
+        name: name,
+        description: description,
+        private: false,
+        image_url: imageURL
       })
-      .then((res) => {
-        navigate("/profile");
-      });
+      .then(() => navigate("/profile"));
   };
 
   return (
-    <section className="create-section">
-      <div className="create-header-box">
-        <h1 className="create-header">Create a BookClub!</h1>
-      </div>
+    <div className="create-club__container">
+      <h1>Create a book club!</h1>
 
-      <div className="form-box">
-        <form onSubmit={(event) => event.preventDefault()}>
-          <div className="form-container">
-            <input
-              className="create-input"
-              type="text"
-              name="name"
-              placeholder="Name your bookclub"
-              onChange={(e) => {
-                setClubName(e.target.value);
-              }}
-            ></input>
-          </div>
+      <form className="create-club__form" onSubmit={event => event.preventDefault()}>
+        <input
+          className="create-club__name"
+          type="text"
+          name="name"
+          maxLength="50"
+          placeholder="Name your club"
+          onChange={event => setName(event.target.value)}
+        ></input>
+        <span className={`create-club__input-counter ${nameCounter === 0 && 'max'}`}>{nameCounter}</span>
 
-          <div className="form-container">
-            <textarea
-              className="create-textarea"
-              type="text"
-              name="description"
-              placeholder="What's your bookclub about?"
-              onChange={(e) => {
-                setClubDescription(e.target.value);
-              }}
-            ></textarea>
-          </div>
-        </form>
+        <textarea
+          className="create-club__description"
+          type="text"
+          name="description"
+          maxLength="200"
+          placeholder="What's your club about?"
+          onChange={event => setDescription(event.target.value)}
+        ></textarea>
+        <span className={`create-club__input-counter ${descriptionCounter === 0 && 'max'}`}>{descriptionCounter}</span>
 
-        <button onClick={create} type="submit" className="create-btn">
-          Create Club!
-        </button>
-      </div>
-    </section>
+        <input
+          className="create-club__image-url"
+          type="text"
+          name="image-url"
+          placeholder="Enter club image URL"
+          onChange={event => setImageURL(event.target.value)}
+        ></input>
+      </form>
+
+      <Button text="Create Book Club" handleClick={createClub} />
+    </div>
   );
 }
